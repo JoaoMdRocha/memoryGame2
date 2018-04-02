c2dd8a2b685d96834f6ff513c88483b0d1618e15
Joao-Rocha <joaomdrocha@gmail.com>
2018-04-02 15:27:47 +0100

    added feature that compares cards that have been flipped

--- js/app.js
+++ js/app.js
@@ -1,21 +1,72 @@
-var ul = document.querySelector('ul');
+const ul = document.querySelector('ul');
+let clickCount =0;
+let compareArray = [];
+let cardTop ;
+
+
+//SHUFFLES CARDS
+
 for (var i = ul.children.length; i >= 0; i--) {
     ul.appendChild(ul.children[Math.random() * i | 0]);
 }
 
 
-
+//FLIPS CARD ON CLICK
 
     var listItems = document.getElementById("board").getElementsByTagName('li');
 
     for (var i=0; i<listItems.length; i++) {
-        listItems[i].addEventListener('click', doStuff, false);
+        listItems[i].addEventListener('click', flipCard, false);
     }
 
-    function doStuff() {
+    function flipCard() {
+//Keeps tracks of number of clicks
+      clickCount = clickCount +1
 
-
-        const cardTop = this.firstElementChild;
+//ADDS "flippedCard" class to cards that have been flipped
+        cardTop = this.firstElementChild;
         cardTop.style.cssText="display:none";
+        this.classList.toggle("flippedCard");
 
+
+
+      if(clickCount == 2){
+//Compares images on cards with "flippedCard as a class"
+        const cardsFlipped = document.getElementsByClassName("flippedCard");
+
+
+        for (i=0;i< cardsFlipped.length; i++){
+
+
+      const checkImg = cardsFlipped[i].firstElementChild;
+      compareArray[i]=checkImg.classList.item(1);
+
+
+        }
+
+
+
+        if(compareArray[0] == compareArray[1]){
+
+          for (i=0;i< cardsFlipped.length; i++){
+
+         cardsFlipped[i].style.backgroundColor = "green";
+
+          }
+
+      }else{
+
+        for (i=0;i< cardsFlipped.length; i++){
+
+       cardsFlipped[i].style.backgroundColor = "red";
+      
+
+        }
+
+        }
+
+
+
+
+      }
     }
